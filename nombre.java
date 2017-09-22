52aa85b106173736b1886f6d7bc13e58ab8775ae
davidrodriguez94 <rodriguezdiezdavid94@gmail.com>
2017-09-22 12:59:07 +0200

    El programa ahora muestra nuestra edad, nombre y lugar de nacimiento.

--- nombre.java
+++ nombre.java
@@ -2,5 +2,6 @@ public class nombre{
 	public static void main(String args[]){
 	System.out.println("David Rodriguez");
 	System.out.println("Tengo 23 años");
+	System.out.println("Nacimiento: León");
  }
 }
